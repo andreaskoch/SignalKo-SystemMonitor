6f7dc65d464c31f6e175149230d39aa996ee82e7
Andreas Koch <andyk7@gmail.com>
2012-09-09 22:02:35 +0200

    Implemented the first draft of the system monitor agent service

---
+++ src/Agent.Service/AgentServiceInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace SignalKo.SystemMonitor.Agent.Service
+{
+    [RunInstaller(true)]
+    public class AgentServiceInstaller : Installer
+    {
+        private ServiceInstaller serviceInstaller;
+        private ServiceProcessInstaller serviceProcessInstaller;
+
+        public AgentServiceInstaller()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.serviceInstaller = new ServiceInstaller();
+            this.serviceProcessInstaller = new ServiceProcessInstaller();
+
+            this.serviceInstaller.Description = "System Monitor Agent";
+            this.serviceInstaller.DisplayName = "SignalKo.SystemMonitor.Agent.Service";
+            this.serviceInstaller.ServiceName = "SignalKo.SystemMonitor.Agent.Service";
+
+            this.serviceProcessInstaller.Account = ServiceAccount.User;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+
+            this.Installers.AddRange(new Installer[] { this.serviceProcessInstaller, this.serviceInstaller });
+        }         
+    }
+}
---
+++ src/Agent.Service/DependencyResolution/StructureMapSetup.cs
@@ -0,0 +1,58 @@
+using SignalKo.SystemMonitor.Agent.Core;
+using SignalKo.SystemMonitor.Agent.Core.Collector;
+using SignalKo.SystemMonitor.Agent.Core.Queueing;
+using SignalKo.SystemMonitor.Agent.Core.Sender;
+using SignalKo.SystemMonitor.Agent.Core.Sender.Configuration;
+using SignalKo.SystemMonitor.Common.Model;
+using SignalKo.SystemMonitor.Common.Services;
+
+using StructureMap;
+
+namespace SignalKo.SystemMonitor.Agent.Service.DependencyResolution
+{
+    public static class StructureMapSetup
+    {
+        public static void Setup()
+        {
+            ObjectFactory.Configure(
+                config =>
+                    {
+                        /* common */
+                        config.For<IEncodingProvider>().Use<DefaultEncodingProvider>();
+                        config.For<IMemoryUnitConverter>().Use<MemoryUnitConverter>();
+                        config.For<ITimeProvider>().Use<UTCTimeProvider>();
+
+                        /* collector */
+                        config.For<ILogicalDiscInstanceNameProvider>().Use<LogicalDiscInstanceNameProvider>();
+                        config.For<IMachineNameProvider>().Use<EnvironmentMachineNameProvider>();
+                        config.For<IProcessorStatusProvider>().Use<ProcessorStatusProvider>();
+
+                        config.For<ISystemStorageStatusProvider>().Use<SystemStorageStatusProvider>();
+                        config.For<ISystemMemoryStatusProvider>().Use<SystemMemoryStatusProvider>();
+                        config.For<ISystemInformationProvider>().Use<SystemInformationProvider>();
+
+                        /* queuing */
+                        var workQueue = new SystemInformationMessageQueue();
+                        var errorQueue = new SystemInformationMessageQueue();
+                        var messageQueueProvider = new SystemInformationMessageQueueProvider(workQueue, errorQueue);
+                        config.For<IMessageQueueProvider<SystemInformation>>().Singleton().Use(() => messageQueueProvider);
+
+                        config.For<IMessageQueuePersistence<SystemInformation>>().Use<JSONSystemInformationMessageQueuePersistence>();
+                        config.For<IJSONMessageQueuePersistenceConfigurationProvider>().Use<AppConfigJSONMessageQueuePersistenceConfigurationProvider>();
+
+                        config.For<IMessageQueueFeeder<SystemInformation>>().Use<SystemInformationMessageQueueFeeder>();
+                        config.For<IMessageQueueWorker<SystemInformation>>().Use<SystemInformationMessageQueueWorker>();
+
+                        /* sender configuration */
+                        config.For<IRESTServiceConfigurationProvider>().Use<AppSettingsRESTServiceConfigurationProvider>();
+
+                        /* sender */
+                        config.For<IRESTClientFactory>().Use<RESTClientFactory>();
+                        config.For<IRESTRequestFactory>().Use<JSONRequestFactory>();
+                        config.For<ISystemInformationSender>().Use<RESTBasedSystemInformationSender>();
+
+                        config.For<ISystemInformationDispatchingService>().Use<SystemInformationDispatchingService>();
+                    });
+        }
+    }
+}
