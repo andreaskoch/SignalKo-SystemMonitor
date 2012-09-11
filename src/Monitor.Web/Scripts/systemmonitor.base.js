aeb6538edaec4bcc1553f45b52600c3e2738e0b0
Andreas Koch <andyk7@gmail.com>
2012-09-11 23:21:20 +0200

    Started adding the chart

--- src/Monitor.Web/Scripts/systemmonitor.base.js
+++ src/Monitor.Web/Scripts/systemmonitor.base.js
@@ -22,7 +22,30 @@
 function MachineStateViewModel(MachineName) {
     var self = this;
     self.MachineName = MachineName;
+    self.ChartTitle = MachineName;
+    self.ChartContainerId = "chart-" + MachineName;
+    self.ChartContainer = ko.observable("<div id='" + self.ChartContainerId + "'></div>");
     self.DataSeries = ko.observableArray();
+
+    self.Series = ko.computed(function() {
+        var series = {};
+
+        var dataSeries = self.DataSeries();
+        for (var i = 0; i < dataSeries.length; i++) {
+            var ds = dataSeries[i];
+
+            var dataSeriesPoints = ds.Points();
+            var values = [];
+            for (var x = 0; dataSeriesPoints.length; i++)
+            {
+                values.push({ "x": dataSeriesPoints[x].Timestamp, "y": dataSeriesPoints[x].Value });
+            }
+
+            series[ds.Name] = values;
+        }
+
+        return series;
+    });
 
     self.GetDataSeriesViewModel = function(Name) {
         var dataSeries = self.DataSeries();
@@ -35,7 +58,52 @@ function MachineStateViewModel(MachineName) {
         }
         
         return null;
-    };    
+    };
+
+    self.chart = null;
+    self.InitializeChart = function() {
+        self.chart = new Highcharts.Chart({
+            chart: {
+                renderTo: self.ChartContainerId,
+                type: 'line',
+                marginRight: 130,
+                marginBottom: 25
+            },
+
+            title: {
+                text: self.ChartTitle,
+                x: -20 //center
+            },
+            xAxis: {
+                categories: []
+            },
+
+            yAxis: {
+                title: {
+                    text: '%'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' + this.x + ': ' + this.y + '%';
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -10,
+                y: 100,
+                borderWidth: 0
+            },
+            series: []
+        });
+    };
 
     self.AddDataSeries = function(Name, Timestamp, Value) {
         var ds = self.GetDataSeriesViewModel(Name);
@@ -82,7 +150,9 @@ function MachineStatesViewModel() {
             {
                 var dataPoint = systemStatus.DataPoints[i];
                 machineStateModel.AddDataSeries(dataPoint.Name, systemStatus.Timestamp, dataPoint.Value);
-            }            
+            }
+
+            machineStateModel.InitializeChart();
         }
     });
 
