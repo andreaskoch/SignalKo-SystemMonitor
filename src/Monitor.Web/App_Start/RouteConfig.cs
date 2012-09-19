f1175d0ed2efb98e39172701f94d2971e00b45c8
Andreas Koch <andyk7@gmail.com>
2012-09-19 22:42:30 +0200

    Added a group configuration controller

--- src/Monitor.Web/App_Start/RouteConfig.cs
+++ src/Monitor.Web/App_Start/RouteConfig.cs
@@ -11,8 +11,12 @@ namespace SignalKo.SystemMonitor.Monitor.Web.App_Start
 
             routes.MapRoute("Default", string.Empty, new { controller = "SystemMonitor", action = "GroupOverview" });
 
+            routes.MapRoute("SystemMonitorDefault", "SystemMonitor", new { controller = "SystemMonitor", action = "GroupOverview" });
             routes.MapRoute("SystemMonitorGroupOverview", "SystemMonitor/GroupOverview", new { controller = "SystemMonitor", action = "GroupOverview" });
             routes.MapRoute("SystemMonitorGroup", "SystemMonitor/Group/{groupName}", new { controller = "SystemMonitor", action = "Group" });
+
+            routes.MapRoute("GroupConfigurationAll", "Configuration/Groups", new { controller = "GroupConfiguration", action = "EditGroups" });
+            routes.MapRoute("GroupConfigurationSpecific", "Configuration/Group/{groupName}", new { controller = "GroupConfiguration", action = "EditGroup" });
         }
     }
 }
---
+++ src/Monitor.Web/Controllers/GroupConfigurationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace SignalKo.SystemMonitor.Monitor.Web.Controllers
+{
+    public class GroupConfigurationController : Controller
+    {
+        public ActionResult EditGroups()
+        {
+            return this.View();
+        }
+
+        public ActionResult EditGroup(string groupName)
+        {
+            return this.View();
+        }
+    }
+}
