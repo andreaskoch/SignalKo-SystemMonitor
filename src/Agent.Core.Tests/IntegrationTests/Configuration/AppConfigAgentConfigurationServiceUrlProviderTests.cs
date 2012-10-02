e2e76868ddd41ed8f877546076832bbec553d5d2
Andreas Koch <andyk7@gmail.com>
2012-10-02 22:40:33 +0200

    Created an integration test for the AppConfigAgentConfigurationServiceUrlProvider

---
+++ src/Agent.Core.Tests/IntegrationTests/Configuration/AppConfigAgentConfigurationServiceUrlProviderTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+using SignalKo.SystemMonitor.Agent.Core.Configuration;
+
+namespace Agent.Core.Tests.IntegrationTests.Configuration
+{
+    [TestFixture]
+    public class AppConfigAgentConfigurationServiceUrlProviderTests
+    {
+        #region GetServiceUrl
+
+        [Test]
+        public void GetServiceUrl_ResultIsNotNullOrEmpty()
+        {
+            // Arrange
+            var appConfigAgentConfigurationServiceUrlProvider = new AppConfigAgentConfigurationServiceUrlProvider();
+
+            // Act
+            var result = appConfigAgentConfigurationServiceUrlProvider.GetServiceUrl();
+
+            // Assert
+            Assert.IsNotNullOrEmpty(result);
+        }
+
+        #endregion
+    }
+}
