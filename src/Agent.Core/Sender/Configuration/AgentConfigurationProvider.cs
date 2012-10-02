04f62fc63383e77055f90f49e5d8fab1f3d25935
Andreas Koch <andyk7@gmail.com>
2012-10-02 20:12:04 +0200

    Added a timer to the agent configuration provider which fetches the agent configuration every n seconds instead of fetching the configuration on every call

--- src/Agent.Core/Sender/Configuration/AgentConfigurationProvider.cs
+++ src/Agent.Core/Sender/Configuration/AgentConfigurationProvider.cs
@@ -1,25 +1,46 @@
 using System;
+using System.Threading;
 
 using SignalKo.SystemMonitor.Common.Model;
 
 namespace SignalKo.SystemMonitor.Agent.Core.Sender.Configuration
 {
-    public class AgentConfigurationProvider : IAgentConfigurationProvider
+    public class AgentConfigurationProvider : IAgentConfigurationProvider, IDisposable
     {
+        private const int DefaultCheckIntervalInSeconds = 60;
+
         private readonly IAgentConfigurationServiceUrlProvider configurationServiceUrlProvider;
 
         private readonly IRESTClientFactory restClientFactory;
 
         private readonly IRESTRequestFactory requestFactory;
 
+        private readonly Timer timer;
+
+        private AgentConfiguration agentConfiguration;
+
         public AgentConfigurationProvider(IAgentConfigurationServiceUrlProvider configurationServiceUrlProvider, IRESTClientFactory restClientFactory, IRESTRequestFactory requestFactory)
         {
             this.configurationServiceUrlProvider = configurationServiceUrlProvider;
             this.restClientFactory = restClientFactory;
             this.requestFactory = requestFactory;
+
+            var timerStartTime = new TimeSpan(0, 0, 0);
+            var timerInterval = new TimeSpan(0, 0, 0, DefaultCheckIntervalInSeconds);
+            this.timer = new Timer(state => this.UpdateAgentConfiguration(), null, timerStartTime, timerInterval);
         }
 
         public AgentConfiguration GetAgentConfiguration()
+        {
+            return this.agentConfiguration;
+        }
+
+        public void Dispose()
+        {
+            this.timer.Dispose();
+        }
+
+        private void UpdateAgentConfiguration()
         {
             var serviceUrl = new Uri(this.configurationServiceUrlProvider.GetServiceUrl());
             string baseUrl = serviceUrl.Scheme + "://" + serviceUrl.Host + ":" + serviceUrl.Port;
@@ -29,9 +50,15 @@ namespace SignalKo.SystemMonitor.Agent.Core.Sender.Configuration
             var request = this.requestFactory.CreateGetRequest(resourcePath);
 
             var response = restClient.Execute<AgentConfiguration>(request);
-            var agentConfiguration = response.Data;
+            this.agentConfiguration = response.Data;
 
-            return agentConfiguration;
+            // update the check interval
+            if (this.agentConfiguration != null && this.agentConfiguration.CheckIntervalInSeconds > 0)
+            {
+                var timerStartTime = new TimeSpan(0, 0, this.agentConfiguration.CheckIntervalInSeconds);
+                var timerInterval = new TimeSpan(0, 0, 0, this.agentConfiguration.CheckIntervalInSeconds);
+                this.timer.Change(timerStartTime, timerInterval);
+            }
         }
     }
 }
