7cc9deb257f61a932a0afa9a828c88c10f0f55f2
Andreas Koch <andyk7@gmail.com>
2012-10-03 12:11:23 +0200

    Created unit- and integration tests for the SystemInformationMessageQueueWorkerFactory

---
+++ src/Agent.Core.Tests/IntegrationTests/Queueing/SystemInformationMessageQueueWorkerFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+
+using NUnit.Framework;
+
+using SignalKo.SystemMonitor.Agent.Core.Queueing;
+using SignalKo.SystemMonitor.Agent.Core.Sender;
+using SignalKo.SystemMonitor.Common.Model;
+
+namespace Agent.Core.Tests.IntegrationTests.Queueing
+{
+    [TestFixture]
+    public class SystemInformationMessageQueueWorkerFactoryTests
+    {
+        #region GetMessageQueueFeeder
+
+        [Test]
+        public void GetMessageQueueWorker_WorkQueueIsNotNull_ErrorQueueIsNotNull_ResultIsNotNull()
+        {
+            // Arrange
+            var systemInformationSender = new Mock<ISystemInformationSender>();
+
+            var workQueue = new Mock<IMessageQueue<SystemInformation>>();
+            var errorQueue = new Mock<IMessageQueue<SystemInformation>>();
+            var messageQueueProvider = new Mock<IMessageQueueProvider<SystemInformation>>();
+            messageQueueProvider.Setup(m => m.WorkQueue).Returns(workQueue.Object);
+            messageQueueProvider.Setup(m => m.ErrorQueue).Returns(errorQueue.Object);
+
+            var systemInformationMessageQueueFeederFactory = new SystemInformationMessageQueueWorkerFactory(systemInformationSender.Object, messageQueueProvider.Object);
+
+            // Act
+            var result = systemInformationMessageQueueFeederFactory.GetMessageQueueWorker();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Agent.Core.Tests/UnitTests/Queueing/SystemInformationMessageQueueWorkerFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Moq;
+
+using NUnit.Framework;
+
+using SignalKo.SystemMonitor.Agent.Core.Queueing;
+using SignalKo.SystemMonitor.Agent.Core.Sender;
+using SignalKo.SystemMonitor.Common.Model;
+
+namespace Agent.Core.Tests.UnitTests.Queueing
+{
+    [TestFixture]
+    public class SystemInformationMessageQueueWorkerFactoryTests
+    {
+        #region Constructor
+
+        [Test]
+        public void Constructor_AllParametersAreSet_ObjectIsInstantiated()
+        {
+            // Arrange
+            var systemInformationSender = new Mock<ISystemInformationSender>();
+            var messageQueueProvider = new Mock<IMessageQueueProvider<SystemInformation>>();
+
+            // Act
+            var systemInformationMessageQueueFeederFactory = new SystemInformationMessageQueueWorkerFactory(
+                systemInformationSender.Object, messageQueueProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(systemInformationMessageQueueFeederFactory);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_SystemInformationSenderParameterIsNull_ArgumentNullExceptionIsThrown()
+        {
+            // Arrange
+            var messageQueueProvider = new Mock<IMessageQueueProvider<SystemInformation>>();
+
+            // Act
+            new SystemInformationMessageQueueWorkerFactory(null, messageQueueProvider.Object);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_MessageQueueProviderParameterIsNull_ArgumentNullExceptionIsThrown()
+        {
+            // Arrange
+            var systemInformationSender = new Mock<ISystemInformationSender>();
+
+            // Act
+            new SystemInformationMessageQueueWorkerFactory(systemInformationSender.Object, null);
+        }
+
+        #endregion
+    }
+}
