71481813313ba6a989ebf51d4d43b506d4a64c30
Andreas Koch <andyk7@gmail.com>
2012-10-03 12:03:56 +0200

    Created unit- and integration tests for the SystemInformationMessageQueueFeederFactory

---
+++ src/Agent.Core.Tests/IntegrationTests/Queueing/SystemInformationMessageQueueFeederFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Moq;
+
+using NUnit.Framework;
+
+using SignalKo.SystemMonitor.Agent.Core.Collector;
+using SignalKo.SystemMonitor.Agent.Core.Queueing;
+using SignalKo.SystemMonitor.Common.Model;
+
+namespace Agent.Core.Tests.IntegrationTests.Queueing
+{
+    [TestFixture]
+    public class SystemInformationMessageQueueFeederFactoryTests
+    {
+        #region GetMessageQueueFeeder
+
+        [Test]
+        public void GetMessageQueueFeeder_WorkQueueIsNotNull_ResultIsNotNull()
+        {
+            // Arrange
+            var systemInformationProvider = new Mock<ISystemInformationProvider>();
+            var workQueue = new Mock<IMessageQueue<SystemInformation>>();
+            var messageQueueProvider = new Mock<IMessageQueueProvider<SystemInformation>>();
+            messageQueueProvider.Setup(m => m.WorkQueue).Returns(workQueue.Object);
+
+            var systemInformationMessageQueueFeederFactory = new SystemInformationMessageQueueFeederFactory(
+                systemInformationProvider.Object, messageQueueProvider.Object);
+
+            // Act
+            var result = systemInformationMessageQueueFeederFactory.GetMessageQueueFeeder();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Agent.Core.Tests/UnitTests/Queueing/SystemInformationMessageQueueFeederFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Moq;
+
+using NUnit.Framework;
+
+using SignalKo.SystemMonitor.Agent.Core.Collector;
+using SignalKo.SystemMonitor.Agent.Core.Queueing;
+using SignalKo.SystemMonitor.Common.Model;
+
+namespace Agent.Core.Tests.UnitTests.Queueing
+{
+    [TestFixture]
+    public class SystemInformationMessageQueueFeederFactoryTests
+    {
+        #region Constructor
+
+        [Test]
+        public void Constructor_AllParametersAreSet_ObjectIsInstantiated()
+        {
+            // Arrange
+            var systemInformationProvider = new Mock<ISystemInformationProvider>();
+            var messageQueueProvider = new Mock<IMessageQueueProvider<SystemInformation>>();
+
+            // Act
+            var systemInformationMessageQueueFeederFactory = new SystemInformationMessageQueueFeederFactory(
+                systemInformationProvider.Object, messageQueueProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(systemInformationMessageQueueFeederFactory);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_SystemInformationProviderParameterIsNull_ArgumentNullExceptionIsThrown()
+        {
+            // Arrange
+            var messageQueueProvider = new Mock<IMessageQueueProvider<SystemInformation>>();
+
+            // Act
+            new SystemInformationMessageQueueFeederFactory(null, messageQueueProvider.Object);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_MessageQueueProviderParameterIsNull_ArgumentNullExceptionIsThrown()
+        {
+            // Arrange
+            var systemInformationProvider = new Mock<ISystemInformationProvider>();
+
+            // Act
+            new SystemInformationMessageQueueFeederFactory(systemInformationProvider.Object, null);
+        }
+
+        #endregion
+    }
+}
