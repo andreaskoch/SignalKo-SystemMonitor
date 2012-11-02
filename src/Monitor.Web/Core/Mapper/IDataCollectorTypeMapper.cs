f6abed5bd289a49cb1c58e7e1635f51b036394e0
Andreas Koch <andyk7@gmail.com>
2012-11-02 21:37:23 +0100

    Changed the implementation of the agent collector definition model class so it can be properly deserialized. Forgot to include the deletes in the last commit.

--- src/Monitor.Web/Core/Mapper/IDataCollectorTypeMapper.cs
+++
@@ -1,9 +0,0 @@
-using SignalKo.SystemMonitor.Common.Model;
-
-namespace SignalKo.SystemMonitor.Monitor.Web.Core.Mapper
-{
-	public interface IDataCollectorTypeMapper
-	{
-		DataCollectorType Map(string collectorType);
-	}
-}
