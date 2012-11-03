7cc80c161ceffb81ab20df9586acc5b6938786bc
Andreas Koch <andyk7@gmail.com>
2012-11-03 21:17:13 +0100

    Quick fix for a RestSharp bug that I cannot explain or fix. RestSharp is not able to deserialize the AgentConfiguration JSON ... whereas Json.Net has no problems.

--- src/Agent.Core/Configuration/AgentConfigurationAccessor.cs
+++ src/Agent.Core/Configuration/AgentConfigurationAccessor.cs
@@ -1,49 +1,62 @@
 using System;
 
+using Newtonsoft.Json;
+
 using SignalKo.SystemMonitor.Agent.Core.Sender;
 using SignalKo.SystemMonitor.Common.Model;
 
 namespace SignalKo.SystemMonitor.Agent.Core.Configuration
 {
-    public class AgentConfigurationAccessor : IAgentConfigurationAccessor
-    {
-        private readonly IAgentConfigurationServiceUrlProvider configurationServiceUrlProvider;
+	public class AgentConfigurationAccessor : IAgentConfigurationAccessor
+	{
+		private readonly IAgentConfigurationServiceUrlProvider configurationServiceUrlProvider;
 
-        private readonly IRESTClientFactory restClientFactory;
+		private readonly IRESTClientFactory restClientFactory;
 
-        private readonly IRESTRequestFactory requestFactory;
+		private readonly IRESTRequestFactory requestFactory;
 
-        public AgentConfigurationAccessor(IAgentConfigurationServiceUrlProvider configurationServiceUrlProvider, IRESTClientFactory restClientFactory, IRESTRequestFactory requestFactory)
-        {
-            if (configurationServiceUrlProvider == null)
-            {
-                throw new ArgumentNullException("configurationServiceUrlProvider");
-            }
+		public AgentConfigurationAccessor(IAgentConfigurationServiceUrlProvider configurationServiceUrlProvider, IRESTClientFactory restClientFactory, IRESTRequestFactory requestFactory)
+		{
+			if (configurationServiceUrlProvider == null)
+			{
+				throw new ArgumentNullException("configurationServiceUrlProvider");
+			}
 
-            if (restClientFactory == null)
-            {
-                throw new ArgumentNullException("restClientFactory");
-            }
+			if (restClientFactory == null)
+			{
+				throw new ArgumentNullException("restClientFactory");
+			}
 
-            if (requestFactory == null)
-            {
-                throw new ArgumentNullException("requestFactory");
-            }
+			if (requestFactory == null)
+			{
+				throw new ArgumentNullException("requestFactory");
+			}
 
-            this.configurationServiceUrlProvider = configurationServiceUrlProvider;
-            this.restClientFactory = restClientFactory;
-            this.requestFactory = requestFactory;            
-        }
+			this.configurationServiceUrlProvider = configurationServiceUrlProvider;
+			this.restClientFactory = restClientFactory;
+			this.requestFactory = requestFactory;
+		}
 
-        public AgentConfiguration GetAgentConfiguration()
-        {
-            var serviceConfiguration = this.configurationServiceUrlProvider.GetServiceConfiguration();
+		public AgentConfiguration GetAgentConfiguration()
+		{
+			var serviceConfiguration = this.configurationServiceUrlProvider.GetServiceConfiguration();
 
-            var restClient = this.restClientFactory.GetRESTClient(serviceConfiguration.Hostaddress);
-            var request = this.requestFactory.CreateGetRequest(serviceConfiguration.ResourcePath, serviceConfiguration.Hostname);
+			var restClient = this.restClientFactory.GetRESTClient(serviceConfiguration.Hostaddress);
+			var request = this.requestFactory.CreateGetRequest(serviceConfiguration.ResourcePath, serviceConfiguration.Hostname);
 
-            var response = restClient.Execute<AgentConfiguration>(request);
-            return response.Data;
-        }
-    }
+			var response = restClient.Execute<AgentConfiguration>(request);
+			string json = response.Content;
+			try
+			{
+				var deserialized = JsonConvert.DeserializeObject<AgentConfiguration>(json);
+				return deserialized;
+			}
+			catch (JsonSerializationException exception)
+			{
+				Console.WriteLine(exception.StackTrace);
+			}
+
+			return null;
+		}
+	}
 }
