00d8c7f533abe1fb6eee5436d091ca336a234bfc
Andreas Koch <andyk7@gmail.com>
2012-11-10 22:10:13 +0100

    Moved the build and package functions into a seperate script library

--- src/Package.ps1
+++ src/Package.ps1
@@ -1,74 +1,15 @@
 ﻿
 # Global Variables
 $currentDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$scriptsDirectory = Join-Path $currentDirectory "scripts"
 $toolsDirectory = Join-Path $currentDirectory "tools"
+
 $nuDeployExecutablePath = Join-Path $toolsDirectory "NuDeploy\NuDeploy.exe"
 $projectDirectory = Split-Path -Parent $currentDirectory
 $publishLocation = Join-Path $projectDirectory "packages"
 
-# Functions
-Function Package-Solution
-{
-   [CmdletBinding()]
-    Param(
-        [Parameter(Position=0, Mandatory=$True, ValueFromPipeline=$True)]
-        [string]$nuDeployExePath,    
-    
-        [Parameter(Position=1, Mandatory=$True, ValueFromPipeline=$True)]
-        [string]$solutionPath,
-		
-        [Parameter(Position=2, Mandatory=$False, ValueFromPipeline=$True)]
-        [string]$publishLocation
-    )
-	
-	if ((Test-Path -Path $nuDeployExePath) -eq $false) {
-		Throw "NuDeploy was not found at `"$nuDeployExePath`""
-	}
-	
-	if ((Test-Path -Path $solutionPath) -eq $false) {
-		Throw "No solution file found at `"$solutionPath`""
-	}
-	
-	$solutionFilename = (Get-Item $solutionPath).FullName
-	Write-Host "Packaging solution `"$solutionFilename`"."
-	
-	$previousLocation = get-location
-	$nuDeployDirectory = Split-Path -Parent $nuDeployExePath
-	$nuDeployPackageCommand = "$nuDeployExecutablePath packagesolution `"-SolutionPath=$solutionPath`" `"-BuildConfiguration=Release`""
-	
-	# Add Publishing Profile
-	if ($publishLocation -ne $null) {
-	
-		if ((Test-Path -Path $publishLocation) -eq $false) {
-			New-Item $publishLocation -type directory
-		}
-	
-		$publishConfigurationName = "Publish"
-		$ensurePublishConfigurationExistsCommand = "$nuDeployExecutablePath targets `"-Action=add`" `"-Name=$publishConfigurationName`" `"-Location=$publishLocation`""
-		set-location $nuDeployDirectory	
-		Invoke-Expression $ensurePublishConfigurationExistsCommand
-		$addPublishConfigurationSucceeded = ($LASTEXITCODE -eq 0)
-		
-		if ($addPublishConfigurationSucceeded -eq $true) {
-			$nuDeployPackageCommand += " `"-PublishingConfiguration=$publishConfigurationName`""
-		}
-	}	
-
-	# Switch to the NuDeploy folder
-	set-location $nuDeployDirectory
-	
-	# Run NuDeploy
-	Invoke-Expression $nuDeployPackageCommand
-	$packageSucceeded = ($LASTEXITCODE -eq 0)
-	
-	# Switch back to the previous location
-	set-location $previousLocation
-
-	if ($packageSucceeded -eq $false)
-	{
-		Throw "Packaging the solution `"$solutionPath`" failed (Command: $nuDeployPackageCommand)."
-	}
-}
+# Imports
+Import-Module (Join-Path $scriptsDirectory "build-utility-functions.ps1")
 
 # Package the Agent
 $solutionPath = Join-Path $currentDirectory "SignalKo.SystemMonitor.Agent.sln"
---
+++ src/scripts/build-utility-functions.ps1
@@ -0,0 +1,121 @@
+﻿
+Function Prepare-Directory {
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position=0, Mandatory=$True, ValueFromPipeline=$True)]
+        [string]$path
+    )
+
+	if ((Test-Path $path) -eq $false) {
+	
+		New-Item $path -type directory
+		
+	} else {
+	
+		if ((Get-ChildItem $path).Count -gt 0)
+		{
+			Remove-Item -Recurse -Force $path
+			if ($? -eq $false) {
+				Throw "Could not cleanup the directory `"$path`""
+			}
+		}
+	
+	}
+	
+}
+
+Function Build-Solution {
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position=0, Mandatory=$True, ValueFromPipeline=$True)]
+        [string]$solutionPath,
+		
+        [Parameter(Position=1, Mandatory=$True, ValueFromPipeline=$True)]
+        [string]$buildOutputFolder,		
+		
+        [Parameter(Position=2, Mandatory=$False, ValueFromPipeline=$True)]
+        [string]$buildConfiguration="Release",
+		
+        [Parameter(Position=3, Mandatory=$False, ValueFromPipeline=$True)]
+        [string]$targetPlatform="Any CPU"
+    )
+	
+	if ((Test-Path $solutionPath) -eq $false) {
+		Throw "The soltuion file `"$solutionPath`" was not found."
+	}
+	
+	Prepare-Directory -path $buildOutputFolder
+
+	$buildCommand = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild `"$solutionPath`" /p:Configuration=`"$buildConfiguration`" /p:Platform=`"$targetPlatform`" /p:OutputPath=`"$buildOutputFolder`" /t:Rebuild"
+	Invoke-Expression $buildCommand
+	$buildSucceeded = ($LASTEXITCODE -eq 0)
+
+	if ($buildSucceeded -eq 0) {
+		Throw "Building solution `"$solutionPath`" failed."
+	}
+}
+
+Function Package-Solution
+{
+   [CmdletBinding()]
+    Param(
+        [Parameter(Position=0, Mandatory=$True, ValueFromPipeline=$True)]
+        [string]$nuDeployExePath,    
+    
+        [Parameter(Position=1, Mandatory=$True, ValueFromPipeline=$True)]
+        [string]$solutionPath,
+		
+        [Parameter(Position=2, Mandatory=$False, ValueFromPipeline=$True)]
+        [string]$publishLocation
+    )
+	
+	if ((Test-Path -Path $nuDeployExePath) -eq $false) {
+		Throw "NuDeploy was not found at `"$nuDeployExePath`""
+	}
+	
+	if ((Test-Path -Path $solutionPath) -eq $false) {
+		Throw "No solution file found at `"$solutionPath`""
+	}
+	
+	$solutionFilename = (Get-Item $solutionPath).FullName
+	Write-Host "Packaging solution `"$solutionFilename`"."
+	
+	$previousLocation = get-location
+	$nuDeployDirectory = Split-Path -Parent $nuDeployExePath
+	$nuDeployPackageCommand = "$nuDeployExecutablePath packagesolution `"-SolutionPath=$solutionPath`" `"-BuildConfiguration=Release`""
+	
+	# Add Publishing Profile
+	if ($publishLocation -ne $null) {
+	
+		if ((Test-Path -Path $publishLocation) -eq $false) {
+			New-Item $publishLocation -type directory
+		}
+	
+		$publishConfigurationName = "Publish"
+		$ensurePublishConfigurationExistsCommand = "$nuDeployExecutablePath targets `"-Action=add`" `"-Name=$publishConfigurationName`" `"-Location=$publishLocation`""
+		set-location $nuDeployDirectory	
+		Invoke-Expression $ensurePublishConfigurationExistsCommand
+		$addPublishConfigurationSucceeded = ($LASTEXITCODE -eq 0)
+		
+		if ($addPublishConfigurationSucceeded -eq $true) {
+			$nuDeployPackageCommand += " `"-PublishingConfiguration=$publishConfigurationName`""
+		}
+	}	
+
+	# Switch to the NuDeploy folder
+	set-location $nuDeployDirectory
+	
+	# Run NuDeploy
+	Invoke-Expression $nuDeployPackageCommand
+	$packageSucceeded = ($LASTEXITCODE -eq 0)
+	
+	# Switch back to the previous location
+	set-location $previousLocation
+
+	if ($packageSucceeded -eq $false)
+	{
+		Throw "Packaging the solution `"$solutionPath`" failed (Command: $nuDeployPackageCommand)."
+	}
+}
